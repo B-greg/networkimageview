fa5c0a8f3cfb8d367859d133ea5f8460b34c6c51
Kitti <kitti@smartsoftasia.com>
2017-05-24 11:15:06 +0700

    Remove fit and centerCrop

--- src/main/java/com/gregb/library/networkimageview/ImageDownloader.java
+++ src/main/java/com/gregb/library/networkimageview/ImageDownloader.java
@@ -48,8 +48,7 @@ public class ImageDownloader {
   public static void loadImage(final Context context, final String url, final ImageView imageView) {
     Picasso.with(context)
         .load(url)
-        .fit()
-
+        //.fit()
         //    .diskCacheStrategy(DiskCacheStrategy.RESULT)
         //    .crossFade()
         .into(imageView);
@@ -59,13 +58,12 @@ public class ImageDownloader {
                                final int downloadIndicator) {
     Picasso.with(context)
         .load(url)
-        .fit()
-        .centerCrop()
-        //    .diskCacheStrategy(DiskCacheStrategy.RESULT)
+        //  .fit()
+        //  .centerCrop()
+        //  .diskCacheStrategy(DiskCacheStrategy.RESULT)
         .placeholder(downloadIndicator)
         //    .crossFade()
         .into(imageView);
   }
-
 
 }
